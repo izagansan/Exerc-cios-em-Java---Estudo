af9f8a8349e68f8cd2bd2d1f9e08d8ca004253e3
izagansan <izabel.gandolfa@gmail.com>
2026-01-19 18:08:52 -0300

    Add files via upload

---
+++ Exercicio23.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DialingCodes {
+
+    private final Map<Integer, String> codes = new HashMap<>();
+
+    public Map<Integer, String> getCodes() {
+        return codes;
+    }
+
+    public void setDialingCode(Integer code, String country) {
+        codes.put(code, country);
+    }
+
+    public String getCountry(Integer code) {
+        return codes.get(code);
+    }
+
+    public void addNewDialingCode(Integer code, String country) {
+        if (codes.containsKey(code)) {
+            return;
+        }
+
+        if (codes.containsValue(country)) {
+            return;
+        }
+
+        codes.put(code, country);
+    }
+
+    public Integer findDialingCode(String country) {
+        for (Map.Entry<Integer, String> entry : codes.entrySet()) {
+            if (entry.getValue().equals(country)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public void updateCountryDialingCode(Integer code, String country) {
+        Integer oldCode = findDialingCode(country);
+
+        if (oldCode == null) {
+            return;
+        }
+
+        codes.remove(oldCode);
+        codes.put(code, country);
+    }
+}
