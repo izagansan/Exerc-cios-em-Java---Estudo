c508bad1c5255df510a1d9ee34f872908b8d09e5
izagansan <izabel.gandolfa@gmail.com>
2026-01-19 14:45:56 -0300

    Add files via upload

---
+++ Exercicio10.java
@@ -0,0 +1,50 @@
+class NeedForSpeed {
+
+    private int speed;
+    private int batteryDrain;
+    private int battery = 100;
+    private int distance = 0;
+
+    NeedForSpeed(int speed, int batteryDrain) {
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
+    }
+
+    public void drive() {
+        if (!batteryDrained()) {
+            distance += speed;
+            battery -= batteryDrain;
+        }
+    }
+
+    public int distanceDriven() {
+        return distance;
+    }
+
+    public boolean batteryDrained() {
+        return battery < batteryDrain;
+    }
+
+    public static NeedForSpeed nitro() {
+        return new NeedForSpeed(50, 4);
+    }
+}
+
+class RaceTrack {
+
+    private int distance;
+
+    RaceTrack(int distance) {
+        this.distance = distance;
+    }
+
+    public boolean canFinishRace(NeedForSpeed car) {
+        while (!car.batteryDrained()) {
+            car.drive();
+            if (car.distanceDriven() >= distance) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
